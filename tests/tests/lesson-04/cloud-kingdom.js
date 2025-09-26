41a20746624831be151fad60814dc357d1d27673
Test 01- Thịnh <alex@playwright.vn.com>
2025-09-26 23:43:32 +0700

    feat: add solution for challenge 03

---
+++ tests/tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,11 @@
+let playerName = "Mario";
+let currentLives = 3;
+const LEVEL_1_COINS = 25;
+const LEVEL_2_COINS = 30;
+const LEVEL_3_COINS = 45;
+const totalCoins = LEVEL_1_COINS + LEVEL_2_COINS + LEVEL_3_COINS;
+const numberOfLevels = 3;
+const averageCoins = totalCoins / numberOfLevels;
+const remainderCoins = totalCoins % 3;
+console.log(`Giá trị trung bình coin: ${averageCoins}`);
+console.log(`Số coin dư khi chia cho 3: ${remainderCoins}`);
