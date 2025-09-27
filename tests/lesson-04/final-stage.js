95c0fc04e51e4b7d2eb1b35915d9a663f2c94737
Test 01- Thịnh <alex@playwright.vn.com>
2025-09-27 12:05:49 +0700

    final stage2

--- tests/lesson-04/final-stage.js
+++ tests/lesson-04/final-stage.js
@@ -1,7 +1,7 @@
 function findPairsDivisibleBy17() {
     let count = 0;
-    for (let i = 1; 1 <= 100; i++) {
-        for (let j = 1; 1 <= 100; j++) {
+    for (let i = 1; i <= 100; i++) {
+        for (let j = 1; j <= 100; j++) {
             if ((i + j) % 17 === 0) {
                 count++;
             }
