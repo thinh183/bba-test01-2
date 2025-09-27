4f4f422f4f2119ebe28e86b5ae7b429ec1d87e87
Test 01- Thịnh <alex@playwright.vn.com>
2025-09-27 12:03:00 +0700

    sửa lỗi file test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://e-commerce-dev.betterbytesvn.com/');
 
   // Expect a title "to contain" a substring.
- await expect(page).toHaveTitle(/“E-commerce site for automationtesting/);
+ await expect(page).toHaveTitle(/E-commerce site for automation testing/i);
 });
 
 test('get started link', async ({ page }) => {
@@ -14,5 +14,5 @@ test('get started link', async ({ page }) => {
   await page.getByRole('link', { name: 'MY ACCOUNT' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'My account' })).toBeVisible();
+  await expect(page).toHaveTitle(/E-commerce site for automation testing/i);
 });
